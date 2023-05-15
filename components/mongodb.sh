5888bde7e6f405201ee1a35f30d9eddc05c97cda
Sandeep <sandeep.vemula88@gmail.com>
2023-05-15 11:47:35 +0530

    Mongodb scripting

--- components/mongodb.sh
+++ components/mongodb.sh
@@ -1,2 +1,23 @@
 #!/usr/bin/env bash
-echo Install mongodb component
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
+yum install -y mongodb-org
+systemctl enable mongod
+systemctl start mongod
+
+#1. Update Listen IP address from 127.0.0.1 to 0.0.0.0 in config file
+
+#Config file: `/etc/mongod.conf`
+
+#then restart the service
+
+systemctl restart mongod
+
+## Every Database needs the schema to be loaded for the application to work.
+
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+cd /tmp
+unzip mongodb.zip
+cd mongodb-main
+mongo < catalogue.js
+mongo < users.js
+
