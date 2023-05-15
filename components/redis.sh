455d57f3b151777f1b5acef8d1e26297106a460c
Sandeep <sandeep.vemula88@gmail.com>
2023-05-15 20:27:27 +0530

    redis module scripting

--- components/redis.sh
+++ components/redis.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
-echo Install redis component
+curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
+yum install redis-6.2.11 -y
+#
+# 2. Update the `bind` from `127.0.0.1` to `0.0.0.0` in config file `/etc/redis.conf` & `/etc/redis/redis.conf`
+
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf
+systemctl enable redis
+systemctl start redis
