6b076163a6e186d682efc4be38b9b0d0336cbac2
Sandeep <sandeep.vemula88@gmail.com>
2023-05-15 07:14:47 +0530

    Roboshop proj setting

--- components/frontend.sh
+++ components/frontend.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
+yum install nginx -y
+systemctl enable nginx
+systemctl start nginx
 
-echo Install frontend component
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+cd /usr/share/nginx/html
+rm -rf *
+unzip /tmp/frontend.zip
+mv frontend-main/static/* .
+mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
+
