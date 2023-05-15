affb2a8a921a4357c3182b831262e8f68691cf11
Sandeep <sandeep.vemula88@gmail.com>
2023-05-15 20:40:12 +0530

    user module scripting

--- components/user.sh
+++ components/user.sh
@@ -1,2 +1,21 @@
 #!/usr/bin/env bash
-echo Install user component
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
+yum install nodejs -y
+useradd roboshop
+curl -s -L -o /tmp/user.zip "https://github.com/roboshop-devops-project/user/archive/main.zip"
+cd /home/roboshop
+rm -rf user
+unzip /tmp/user.zip
+mv user-main user
+cd /home/roboshop/user
+npm install
+
+#1. Update SystemD service file,
+#   Update `REDIS_ENDPOINT` with Redis Server IP
+#  Update `MONGO_ENDPOINT` with MongoDB Server IP
+
+sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' /home/roboshop/user/systemd.service
+mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service
+systemctl daemon-reload
+systemctl start user
+systemctl enable user
