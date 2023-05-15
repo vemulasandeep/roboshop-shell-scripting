fb5e6dc3a05802d8db454305481cbe7c362f4e5c
Sandeep <sandeep.vemula88@gmail.com>
2023-05-15 18:36:13 +0530

    catalogue scripting

--- components/catalogue.sh
+++ components/catalogue.sh
@@ -1,2 +1,23 @@
 #!/usr/bin/env bash
-echo Install catalogue component
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
+yum install nodejs -y
+useradd roboshop
+
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
+cd /home/roboshop
+unzip /tmp/catalogue.zip
+mv catalogue-main catalogue
+cd /home/roboshop/catalogue
+npm install
+
+#1. Update SystemD file with correct IP addresses
+
+  #  Update `MONGO_DNSNAME` with MongoDB Server IP
+
+#2. Now, lets set up the service with systemctl.
+
+mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+systemctl daemon-reload
+systemctl start catalogue
+systemctl enable catalogue
+
