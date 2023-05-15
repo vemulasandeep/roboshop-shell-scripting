4113643879b48f70523427669a6ed0b9d64c0529
Sandeep <sandeep.vemula88@gmail.com>
2023-05-15 20:51:46 +0530

    systemfile update

--- components/frontend.sh
+++ components/frontend.sh
@@ -9,6 +9,6 @@ rm -rf *
 unzip /tmp/frontend.zip
 mv frontend-main/static/* .
 mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
-sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
+sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.internal/' -e '/user/ s/localhost/user.roboshop.internal/'/etc/nginx/default.d/roboshop.conf
 
 systemctl restart nginx
