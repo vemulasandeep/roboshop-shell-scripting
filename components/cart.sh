ef0edf43c0ee6ff8ab91385aa17d8f8e8680db32
Sandeep <sandeep.vemula88@gmail.com>
2023-05-15 21:49:16 +0530

    cart module scripting

--- components/cart.sh
+++ components/cart.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
 
-echo Install cart component
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
+yum install nodejs -y
+useradd roboshop
+curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
+cd /home/roboshop
+rm -rf user
+unzip /tmp/cart.zip
+mv cart-main cart
+cd cart
+npm install
+
+sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/'/home/roboshop/cart/systemd.service
+mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
+systemctl daemon-reload
+systemctl start cart
+systemctl enable cart
