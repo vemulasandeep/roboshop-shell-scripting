c753bacfb8f807353cb4dff7ebdf0ade1d39630e
Sandeep <sandeep.vemula88@gmail.com>
2023-05-16 18:46:18 +0530

    dispatch module scripting

--- components/dispatch.sh
+++ components/dispatch.sh
@@ -9,3 +9,7 @@ cd dispatch
 go mod init dispatch
 go get
 go build
+mv /home/roboshop/dispatch/systemd.service /etc/systemd/system/dispatch.service
+systemctl daemon-reload
+systemctl enable dispatch
+systemctl start dispatch
