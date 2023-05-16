e53d5d79884a6d0d177042f874b904a7b5c6d14f
Sandeep <sandeep.vemula88@gmail.com>
2023-05-16 17:36:32 +0530

    shipping module scripting

--- components/shipping.sh
+++ components/shipping.sh
@@ -1,2 +1,18 @@
 #!/usr/bin/env bash
-echo Install shipment component
+yum install maven -y
+useradd roboshop
+cd /home/roboshop
+rm -rf shipping
+curl -s -L -o /tmp/shipping.zip "https://github.com/roboshop-devops-project/shipping/archive/main.zip"
+unzip /tmp/shipping.zip
+mv shipping-main shipping
+cd shipping
+mvn clean package
+mv target/shipping-1.0.jar shipping.jar
+sed -i -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/' /home/roboshop/shipping/systemd.service
+mv /home/roboshop/shipping/systemd.service /etc/systemd/system/shipping.service
+systemctl daemon-reload
+systemctl start shipping
+systemctl enable shipping
+
+
