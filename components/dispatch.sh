18c71575f4e21db792dc7515a40b68657ae2c9c5
Sandeep <sandeep.vemula88@gmail.com>
2023-05-16 18:38:21 +0530

    dispatch module scripting

--- components/dispatch.sh
+++ components/dispatch.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env bash
-echo Install dispatch component
+yum install golang -y
+useradd roboshop
+curl -L -s -o /tmp/dispatch.zip https://github.com/roboshop-devops-project/dispatch/archive/refs/heads/main.zip
+rm -rf dispatch
+unzip /tmp/dispatch.zip
+mv dispatch-main dispatch
+cd dispatch
+go mod init dispatch
+go get
+go build
