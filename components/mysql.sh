b70af1646316c5eed26f0b022d3957879ddfee94
Sandeep <sandeep.vemula88@gmail.com>
2023-05-16 17:07:46 +0530

    mysql module scripting

--- components/mysql.sh
+++ components/mysql.sh
@@ -6,20 +6,9 @@ systemctl start mysqld
 MySQL_DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" | mysql --connect-expired-password -uroot -p"${MySQL_DEFAULT_PASSWORD}"
 echo "uninstall plugin validate_password;" | mysql -uroot -p"${MYSQL_PASSWORD}"
-#```
-#
-### **Setup Needed for Application.**
-#
-#As per the architecture diagram, MySQL is needed by
-#
-#- Shipping Service
-#
-#So we need to load that schema into the database, So those applications will detect them and run accordingly.
-#
-#To download schema, Use the following command
-#
-#```bash
-## curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
-#```
-#
-#Load the schema for Services.
+
+curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
+cd /tmp
+unzip -o mysql.zip
+cd mysql-main
+mysql -u root -pRoboShop@1 <shipping.sql
