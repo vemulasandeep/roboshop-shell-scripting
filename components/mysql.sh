5bc2ba4f5ae7e40451184ac3b06037f77fa26b68
Sandeep <sandeep.vemula88@gmail.com>
2023-05-16 12:38:55 +0530

    mysql module scripting

--- components/mysql.sh
+++ components/mysql.sh
@@ -6,30 +6,8 @@ systemctl start mysqld
 MySQL_DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" > /tmp/mysql
 mysql --connect-expired-password -uroot -p"${MySQL_DEFAULT_PASSWORD}" < /tmp/mysql
-#1. Now a default root password will be generated and given in the log file.
-#
-#```bash
-## grep temp /var/log/mysqld.log
-#```
-#
-#1. Next, We need to change the default root password in order to start using the database service. Use password `RoboShop@1` or any other as per your choice. Rest of the options you can choose `No`
-#
-#```bash
-## mysql_secure_installation
-#```
-#
-#1. You can check the new password working or not using the following command in MySQL
-#
-#First lets connect to MySQL
-#
-#```bash
-## mysql -uroot -pRoboShop@1
-#```
-#
-#Once after login to MySQL prompt then run this SQL Command.
-#
-#```sql
-#> uninstall plugin validate_password;
+echo "uninstall plugin validate_password;" | mysql -uroot -p"${MySQL_DEFAULT_PASSWORD}"
+
 #```
 #
 ### **Setup Needed for Application.**
