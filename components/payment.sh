fdbec521f3a29742e875450059aba6745a13e73b
Sandeep <sandeep.vemula88@gmail.com>
2023-05-16 17:58:24 +0530

    payment module scripting

--- components/payment.sh
+++ components/payment.sh
@@ -1,2 +1,13 @@
 #!/usr/bin/env bash
-echo Install payment component
+yum install python36 gcc python3-devel -y
+useradd roboshop
+cd /home/roboshop
+rm -rf payment
+curl -L -s -o /tmp/payment.zip "https://github.com/roboshop-devops-project/payment/archive/main.zip"
+unzip /tmp/payment.zip
+mv payment-main payment
+cd /home/roboshop
+curl -L -s -o /tmp/payment.zip "https://github.com/roboshop-devops-project/payment/archive/main.zip"
+unzip /tmp/payment.zip
+mv payment-main payment
+
