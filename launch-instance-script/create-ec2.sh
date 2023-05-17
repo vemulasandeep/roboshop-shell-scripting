b278fc5b7551938ab0f9642c58c039e83c168f06
Sandeep <sandeep.vemula88@gmail.com>
2023-05-17 17:58:29 +0530

    ec2-launch-script

--- launch-instance-script/create-ec2.sh
+++ launch-instance-script/create-ec2.sh
@@ -10,7 +10,6 @@ create_ec2() {
       --image-id ${AMI_ID} \
       --instance-type t2.micro \
       --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
-      --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"\
       --security-group-ids ${SGID} \
       | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
