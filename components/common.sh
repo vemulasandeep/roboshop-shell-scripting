b1606aad97b4fb851d482ff890b26b55d1dea929
Sandeep <sandeep.vemula88@gmail.com>
2023-05-18 16:02:20 +0530

    sourcing common file

--- components/common.sh
+++ components/common.sh
@@ -4,8 +4,7 @@ CHECK_ROOT() {
 USER_ID=$(id -u)
 if [ $USER_ID -ne 0 ]
 then
-  echo you are not a root user
-  echo You can run this script as root user or sudo
-  exit 1
+  echo -e "\e[33m You should be running this script as root user or sudo the script \e[0m "
+    exit 1
 fi
 }
