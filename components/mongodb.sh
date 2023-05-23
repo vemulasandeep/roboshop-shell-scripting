1ea9492c7a201e03b18d4afcd9abb9231270b83b
Sandeep <sandeep.vemula88@gmail.com>
2023-05-23 18:06:46 +0530

    sourcing common file

--- components/mongodb.sh
+++ components/mongodb.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 source components/common.sh
 CHECK_ROOT
-curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
-yum install -y mongodb-org
-systemctl enable mongod
-systemctl start mongod
+
+PRINT "Configure YUM repos"
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>${LOG}
+CHECK_STAT $?
+
+PRINT "Install mongodb"
+yum install -y mongodb-org &>>${LOG}
+CHECK_STAT $?
+
+
 
 #1. Update Listen IP address from 127.0.0.1 to 0.0.0.0 in config file
 
@@ -13,17 +19,21 @@ systemctl start mongod
 #then restart the service
 
 #vim /etc/mongod.conf
+PRINT "Configure mongodb service"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${LOG}
+CHECK_STAT $?
 
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+PRINT "Start Mongodb"
+systemctl enable mongod &>>${LOG} && systemctl start mongod &>>${LOG}
+CHECK_STAT $?
 
-systemctl restart mongod
 
 ## Every Database needs the schema to be loaded for the application to work.
+PRINT "Download the schema"
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>${LOG}
+CHECK_STAT $?
 
-curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
-cd /tmp
-unzip -o mongodb.zip
-cd mongodb-main
-mongo < catalogue.js
-mongo < users.js
+PRINT "Load the Schema"
+cd /tmp && unzip -o mongodb.zip &>>${LOG} && cd mongodb-main && mongo < catalogue.js &>>${LOG} && mongo < users.js &>>${LOG}
+CHECK_STAT $?
 
