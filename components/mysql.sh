0454b3f317e680c2cad6c070d00748032163d1e4
Sandeep <sandeep.vemula88@gmail.com>
2023-05-23 18:32:02 +0530

    sourcing common file

--- components/mysql.sh
+++ components/mysql.sh
@@ -4,7 +4,7 @@ CHECK_ROOT
 
 PRINT "Configure YUM repos"
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>${LOG}
-CHECK_STAT &?
+CHECK_STAT $?
 
 PRIINT "Install my sql "
 yum install mysql-community-server -y &>>${LOG}
