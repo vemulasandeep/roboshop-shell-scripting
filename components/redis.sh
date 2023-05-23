3ef963702cc18afcce44de5b4166cebb93243d10
Sandeep <sandeep.vemula88@gmail.com>
2023-05-23 19:26:53 +0530

    sourcing common file

--- components/redis.sh
+++ components/redis.sh
@@ -3,15 +3,19 @@ source components/common.sh
 CHECK_ROOT
 PRINT "Install Yum repos"
 curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>>${LOG}
+CHECK_STAT $?
 
 PRINT "Install redis"
 yum install redis-6.2.11 -y &>>${LOG}
+CHECK_STAT $?
 #
 # 2. Update the `bind` from `127.0.0.1` to `0.0.0.0` in config file `/etc/redis.conf` & `/etc/redis/redis.conf`
 
 
 PRINT "Update configure file"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>${LOG}
+CHECK_STAT $?
 
 PRINT "Statrt service file"
 systemctl enable redis && systemctl start redis
+CHECK_STAT $?
