0de07814403d1a6ebe926d17d2bb42b184b25ea3
Sandeep <sandeep.vemula88@gmail.com>
2023-05-23 18:30:52 +0530

    sourcing common file

--- components/mysql.sh
+++ components/mysql.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/env bash
 source components/common.sh
 CHECK_ROOT
-curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
-yum install mysql-community-server -y
-systemctl enable mysqld
-systemctl start mysqld
-MySQL_DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" | mysql --connect-expired-password -uroot -p"${MySQL_DEFAULT_PASSWORD}"
-echo "uninstall plugin validate_password;" | mysql -uroot -p"${MYSQL_PASSWORD}"
 
-curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
-cd /tmp
-unzip -o mysql.zip
-cd mysql-main
-mysql -u root -p"${MYSQL_PASSWORD}" <shipping.sql
+PRINT "Configure YUM repos"
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>${LOG}
+CHECK_STAT &?
+
+PRIINT "Install my sql "
+yum install mysql-community-server -y &>>${LOG}
+CHECK_STAT $?
+
+PRINT "Starting MySQL service"
+systemctl enable mysqld &>>${LOG} && systemctl start mysqld &>>${LOG}
+CHECK_STAT $?
+
+MySQL_DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
+
+PRINT "Reset root password"
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" | mysql --connect-expired-password -uroot -p"${MySQL_DEFAULT_PASSWORD}"
+CHECK_STAT $?
+
+exit
