39370035a5a0c75dc40964feaf591219faed703c
Sandeep <sandeep.vemula88@gmail.com>
2023-05-23 17:32:48 +0530

    sourcing common file

--- components/common.sh
+++ components/common.sh
@@ -101,7 +101,7 @@ rm -rf * &>>${LOG}
 CHECK_STAT $?
 
 PRINT "extract${COMPONENT} content"
-unzip /tmp/${COMPONENT}.zip
+unzip /tmp/${COMPONENT}.zip &>>${LOG}
 CHECK_STAT $?
 
 PRINT "Organize ${COMPONENT} content"
