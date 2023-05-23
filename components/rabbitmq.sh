9a563180105934a8437c04660342fe50b4c3ad04
Sandeep <sandeep.vemula88@gmail.com>
2023-05-23 19:46:02 +0530

    sourcing common file

--- components/rabbitmq.sh
+++ components/rabbitmq.sh
@@ -2,6 +2,11 @@
 source components/common.sh
 CHECK_ROOT
 
+if [ -z "$RABBITMQ_USER_PASSWORD" ]; then
+echo "Env variable password required"
+exit 1
+fi
+
 PRINT "Setup YUM rabbitmq repos"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo bash &>>${LOG}
 CHECK_STAT $?
@@ -10,12 +15,20 @@ PRINT "Setup erlang repos"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>${LOG}
 CHECK_STAT $?
 
-PRINT "Install erlang & Rabbitmq"
+PRINT "Installing erlang & Rabbitmq"
 yum install erlang  rabbitmq-server -y &>>${LOG}
+CHECK_STAT $?
 
+PRINT "Start rabbitmq server"
+systemctl enable rabbitmq-server && systemctl start rabbitmq-server
+CHECK_STAT $?
 
-#systemctl enable rabbitmq-server
-#systemctl start rabbitmq-server
-#rabbitmqctl add_user roboshop $"{RABBITMQ_USER_PASSWORD}"
-#rabbitmqctl set_user_tags roboshop administrator
-#rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+PRINT "create rabbitmq user"
+rabbitmqctl add_user roboshop $"{RABBITMQ_USER_PASSWORD}" &>>${LOG}
+CHECK_STAT $?
+
+PRINT "Rabbitmq user tags and permissions"
+
+rabbitmqctl set_user_tags roboshop administrator &>>${LOG} && rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>${LOG}
+CHECK_STAT $?
+
