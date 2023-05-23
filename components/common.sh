e545532e5d668f1409b9d8fb0b9662d2ed17e817
Sandeep <sandeep.vemula88@gmail.com>
2023-05-23 11:33:29 +0530

    sourcing common file

--- components/common.sh
+++ components/common.sh
@@ -4,7 +4,7 @@ CHECK_ROOT() {
 USER_ID=$(id -u)
 if [ $USER_ID -ne 0 ]
 then
-  echo -e "\e[33m You should be running this script as root user or sudo the script \e[0m "
+  echo -e "\e[34m You should be running this script as root user or sudo the script \e[0m "
     exit 1
 fi
 }
