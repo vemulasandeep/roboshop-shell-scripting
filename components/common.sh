5541e72598c0566cef50a21d386cfbfca6e5a2b3
Sandeep <sandeep.vemula88@gmail.com>
2023-05-27 15:54:47 +0530

    Shell scripting refinement

--- components/common.sh
+++ components/common.sh
@@ -53,7 +53,7 @@ APP_COMMON_SETUP() {
 
 SYSTEMD() {
 PRINT  "Update SystemD configuration"
-sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal'/  -e 's/MONGO_DNSNAME/mogodb.roboshop.internal'/ -e 's/CARTHOST/cart.roboshop.internal/' -e 's/USERHOST/user.roboshop.internal/' -e 's/AMQPHOST/rabbitmq.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service &>>${LOG}
+sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal'/  -e 's/MONGO_DNSNAME/mongodb.roboshop.internal'/ -e 's/CARTHOST/cart.roboshop.internal/' -e 's/USERHOST/user.roboshop.internal/' -e 's/AMQPHOST/rabbitmq.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service &>>${LOG}
 CHECK_STAT $?
 
 PRINT  "setup SystemD configurations"
